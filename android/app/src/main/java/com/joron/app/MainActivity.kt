2a78f6f82ca79cf630e2b8231f246e0ed4e1c65d
muyeedsarker <sarkermuyeed@gmail.com>
2026-08-21 03:28:07 +0600

    feat(android): initialize Firebase on app startup

--- android/app/src/main/java/com/joron/app/MainActivity.kt
+++ android/app/src/main/java/com/joron/app/MainActivity.kt
@@ -2,10 +2,15 @@ package com.joron.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize Firebase using android/app/google-services.json.
+        FirebaseApp.initializeApp(this)
+
         setContentView(R.layout.activity_main)
     }
 }

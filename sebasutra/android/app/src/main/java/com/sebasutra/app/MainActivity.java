0eb982ddfa1191c013212d8f506c259cdd987851
muyeedsarker <sarkermuyeed@gmail.com>
2026-08-20 07:35:52 +0600

    feat(android): add Sebasutra WebView launcher

---
+++ sebasutra/android/app/src/main/java/com/sebasutra/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sebasutra.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String APP_URL = "https://raw.githubusercontent.com/muyeedsarker/JORON/sebasutra-mvp/sebasutra/app.html";
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        web.loadUrl(APP_URL);
+        setContentView(web);
+    }
+    @Override public void onBackPressed() {
+        WebView web = (WebView) findViewById(android.R.id.content).getRootView().findViewById(android.R.id.content);
+        super.onBackPressed();
+    }
+}

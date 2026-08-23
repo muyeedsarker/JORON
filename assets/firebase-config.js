8ff984d40df3a4699374f9bc56e8cb2dbdfb4c3b
muyeedsarker <sarkermuyeed@gmail.com>
2026-08-23 20:27:53 +0600

    JORON: Add shared Firebase config

---
+++ assets/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDWxxu3zwThJp1fuWMhXiRig3Fswt0QARA",
+  authDomain: "joron-d7742.firebaseapp.com",
+  projectId: "joron-d7742",
+  storageBucket: "joron-d7742.firebasestorage.app",
+  messagingSenderId: "907727989078",
+  appId: "1:907727989078:web:4fdd266e3517a862fa0ef7",
+  measurementId: "G-Q8PXH6C5NC"
+};
